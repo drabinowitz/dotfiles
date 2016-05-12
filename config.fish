9b54618a024f1d1cb26b34b4a1b5c310325faef5
drabinowitz <dmitrirabinowitz+code@gmail.com>
2016-05-12 13:39:05 -0400

    Remove access key

--- config.fish
+++ config.fish
@@ -2,8 +2,6 @@ set -x JAVA_HOME (/usr/libexec/java_home)
 set -g -x PATH /usr/local/bin $PATH
 set -g -x fish_greeting ''
 set -g -x FZF_TMUX 0
-set -g -x SAUCE_USERNAME drabinowitz
-set -g -x SAUCE_ACCESS_KEY efb58153-4285-437b-873f-d64a0f50d5f5
 set -g -x GOPATH /Users/dmitrirabinowitz/gopath/
 fish_vi_mode
 function my_vi_bindings
