183a664f69caf6e28054e916446a6d898674658d
drabinowitz <dmitrirabinowitz+code@gmail.com>
2016-05-20 12:04:32 -0400

    Have a buggy implementation need to figure out issue with new apps not working

--- .slate.js
+++ .slate.js
@@ -45,20 +45,46 @@ slate.configAll({
 //slate.bind('j:ctrl', focusUnlessItermFactory(focusDown, sendDownKeys));
 //slate.bind('k:ctrl', focusUnlessItermFactory(focusUp, sendUpKeys));
 
-function throwOp (screenId, screenSizeX, screenSizeY, screenOriginX, screenOriginY) {
+function throwOp (appName, screenId, screenSizeX, screenSizeY, screenOriginX, screenOriginY) {
     screenSizeX = screenSizeX || 'screenSizeX';
     screenSizeY = screenSizeY || 'screenSizeY';
 
     screenOriginX = screenOriginX || 'screenOriginX';
     screenOriginY = screenOriginY || 'screenOriginY';
 
-    return slate.operation('throw', {
+    var slateThrowOp = slate.operation('throw', {
         'screen': screenId,
         'width': screenSizeX,
         'height': screenSizeY,
         'x': screenOriginX,
         'y': screenOriginY
     });
+
+    return function () {
+        var lowestPidAppWindow = null;
+        var lowestPid = Infinity;
+        slate.eachApp(function (appObject) {
+            var mainWindow = appObject.mainWindow();
+            if (appObject.name() === appName && mainWindow) {
+                var pid = appObject.pid();
+                if (pid < lowestPid) {
+                    lowestPidAppWindow = mainWindow;
+                    lowestPid = pid;
+                }
+            }
+        });
+        if (lowestPidAppWindow) {
+            lowestPidAppWindow.doOperation(slateThrowOp);
+        }
+    };
+}
+
+function layoutThrowOp (appName) {
+    var obj = {};
+    obj[appName] = {
+        'operations': [throwOp.apply(null, arguments)]
+    };
+    return obj;
 }
 
 function getSlateLayoutRecord (name, params) {
@@ -70,67 +96,37 @@ function getSlateLayoutRecord (name, params) {
     }
 }
 
+var iTermLayout = getSlateLayoutRecord('iTerm', layoutThrowOp('iTerm', '1'));
+
+var spotifyLayout = getSlateLayoutRecord('Spotify', layoutThrowOp('Spotify', '0'));
+
+var googleChromeLayout = getSlateLayoutRecord('Google Chrome', layoutThrowOp('Google Chrome', '0'));
+
+var intellijIdeaLayout = getSlateLayoutRecord('IntelliJ IDEA', layoutThrowOp('IntelliJ IDEA', '0'));
+
+var googleChromeITermLayout = getSlateLayoutRecord('Google Chrome iTerm',
+    Object.assign(
+        layoutThrowOp('Google Chrome', '0', 'screenSizeX/2'),
+        layoutThrowOp('iTerm', '0', 'screenSizeX/2', 'screenSizeY', 'screenOriginX+newWindowSizeX'))
+);
+
+var isDirty = false;
 var relaunch = slate.operation('relaunch');
-
-var iTermLayout = getSlateLayoutRecord('iTerm', {
-    'iTerm': {
-        'operations': [throwOp('1')]
+var relaunchIfDirty = function () {
+    if (isDirty) {
+        isDirty = false;
+        relaunch.run();
     }
-});
+};
 
-var spotifyLayout = getSlateLayoutRecord('Spotify', {
-    'Spotify': {
-        'operations': [throwOp('0')]
-    }
-});
+function runBeforeThrowLayoutOp (appName) {
+    return Object.assign({
+        '_before_': {
+            'operations': [runIfOp(appName), relaunchIfDirty]
+        }
 
-var googleChromeLayout = getSlateLayoutRecord('Google Chrome', {
-    'Google Chrome': {
-        'operations': [throwOp('0')]
-    }
-});
-
-var googleChromeITermLayout = getSlateLayoutRecord('Google Chrome iTerm', {
-    'Google Chrome': {
-        'operations': [throwOp('0', 'screenSizeX/2')]
-    },
-
-    'iTerm': {
-        'operations': [
-            throwOp('0', 'screenSizeX/2', 'screenSizeY', 'screenOriginX+newWindowSizeX')
-        ]
-    }
-});
-
-var mainGoogleChromeLayout = getSlateLayoutRecord('Main:Google Chrome', {
-    'Google Chrome': {
-        'operations': [throwOp('1')]
-    },
-
-    '_after_': {
-        'operations': [relaunch]
-    }
-});
-
-var mainFirefoxLayout = getSlateLayoutRecord('Main:Firefox', {
-    'Firefox': {
-        'operations': [throwOp('1')]
-    },
-
-    '_after_': {
-        'operations': [relaunch]
-    }
-});
-
-var mainSafariLayout = getSlateLayoutRecord('Main:Safari', {
-    'Safari': {
-        'operations': [throwOp('1')]
-    },
-
-    '_after_': {
-        'operations': [relaunch]
-    }
-});
+    }, layoutThrowOp.apply(null, arguments));
+}
 
 var runIfMap = {
     'iTerm': '/Applications/iTerm 2.app',
@@ -139,32 +135,46 @@ var runIfMap = {
     'Safari': '/Applications/Safari.app',
     'Spotify': '/Applications/Spotify.app'
 };
-function runIf(name) {
-    var running = false;
-    slate.eachApp(function (app) {
-        if (app.name() == name) {
-            running = true;
+function runIfOp (name) {
+    return function () {
+        var running = false;
+        slate.eachApp(function (app) {
+            if (app.name() == name) {
+                running = true;
+            }
+        });
+        if (!running) {
+            isDirty = true;
+            slate.shell('/usr/bin/open ' + '"' + runIfMap[name] + '"', true, '~/');
         }
-    });
-    if (!running) {
-      slate.shell('/usr/bin/open ' + '"' + runIfMap[name] + '"', true, '~/');
-    }
+    };
 }
 
-function makeLayoutOp (layoutRecord) {
-    return function () {
-        for (var app in layoutRecord.params) {
-            if (layoutRecord.params.hasOwnProperty(app)) {
-                if (app !== '_before_' || app !== '_after_') {
-                    runIf(app)
-                }
-            }
-        }
+var mainGoogleChromeLayout = getSlateLayoutRecord('Main:Google Chrome', runBeforeThrowLayoutOp('Google Chrome', '1')
 
-        slate.operation('layout', {
-            'name': slate.layout(layoutRecord.name, layoutRecord.params)
-        }).run();
-    };
+    //'_after_': {
+    //    'operations': [relaunchIfDirty]
+    //}
+);
+
+var mainFirefoxLayout = getSlateLayoutRecord('Main:Firefox', runBeforeThrowLayoutOp('Firefox', '1')
+
+    //'_after_': {
+    //    'operations': [relaunchIfDirty]
+    //}
+);
+
+var mainSafariLayout = getSlateLayoutRecord('Main:Safari', runBeforeThrowLayoutOp('Safari', '1')
+
+    //'_after_': {
+    //    'operations': [relaunchIfDirty]
+    //}
+);
+
+function makeLayoutOp (layoutRecord) {
+    return slate.operation('layout', {
+        'name': slate.layout(layoutRecord.name, layoutRecord.params)
+    });
 }
 
 slate.bind('n:ctrl', makeLayoutOp(iTermLayout));
@@ -172,6 +182,8 @@ slate.bind('i:ctrl', makeLayoutOp(googleChromeLayout));
 
 slate.bind('n:cmd;ctrl', makeLayoutOp(spotifyLayout));
 slate.bind('m:cmd;ctrl', makeLayoutOp(googleChromeITermLayout));
+slate.bind('u:cmd;ctrl', makeLayoutOp(intellijIdeaLayout));
+
 slate.bind('i:cmd;ctrl', makeLayoutOp(mainGoogleChromeLayout));
 slate.bind('o:cmd;ctrl', makeLayoutOp(mainFirefoxLayout));
 slate.bind('p:cmd;ctrl', makeLayoutOp(mainSafariLayout));
